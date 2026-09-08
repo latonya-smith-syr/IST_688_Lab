ed93974a7ffff828e7f0b4942ecb519e151b53c1
latonya-smith-syr <lsmit127@syr.edu>
2026-09-08 16:10:37 +0000

    adding the bufer code and the system instructions

--- Pages/lab_03.py
+++ Pages/lab_03.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 
 st.title("My Lab 3 question answering chatbot")
 
-st.write("Hi! We will be using Chat-GPT for this bot")
+st.write("Chatbot Demo")
 
 model = "gpt-4o-mini"
 
@@ -13,3 +13,35 @@ if 'client' not in st.session_state:
 
 if "messgaes" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    
+for msg in st.session_state.messages:
+    chat_msg = st.chat_message(msg["role"])
+    chat_msg.write(msg["content"])
+
+def buffer(messages):
+    k = 0
+    while k <=5:
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        k += 1
+
+if prompt := st.chat_input("What is up?"):    
+    st.session_state.messages.append({"role": "system", "content": "After the user's first response ask them this:Do you want more information?. "
+        "If they say yes, give them more information and then ask them specifically: Do you want more information?. If they say no, ask them specifically How can I help you?"})
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    buffer(st.session_state.messages)
+
+    client = st.session_state.client
+    stream = client.chat.completions.create(
+        model= model,
+        messages = st.session_state.messages,
+        stream=True
+    )
+
+    with st.chat_message("assistant"):
+        response = st.write_stream(stream)
+
+    
