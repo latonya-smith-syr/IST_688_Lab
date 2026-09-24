3532db4a303c3a64fe3da6761e9cd0a07d8417d6
latonya-smith-syr <lsmit127@syr.edu>
2026-09-24 18:15:40 +0000

    created lab 5 page

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,9 @@ st.set_page_config(
 lab_01 = st.Page('Pages/lab_01.py', title="Lab 1")
 lab_02 = st.Page('Pages/lab_02.py', title = "Lab 2")
 lab_03 = st.Page('Pages/lab_03.py', title= "Lab 3")
-lab_04 = st.Page('Pages/lab_04.py', title= "Lab 4", default=True)
+lab_04 = st.Page('Pages/lab_04.py', title= "Lab 4")
+lab_05 = st.Page('Pages/lab_05.py', title= "Lab 5", default = True)
 
-pg = st.navigation([lab_01, lab_02, lab_03, lab_04])
+pg = st.navigation([lab_01, lab_02, lab_03, lab_04, lab_05])
 st.set_page_config(page_title='Lab Manager')
 pg.run()
